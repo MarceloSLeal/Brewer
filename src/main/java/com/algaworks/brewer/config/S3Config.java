b11172aca59067ddd9f6bf0db1a2265ab5d744a9
Unknown <marcelosleal@outlook.com>
2018-04-08 13:12:01 -0300

    Profiles local e web(AWS)
    mudanças para preparar o carregamento de imagens por meios de urls, criação de Profiles no Spring para carregar imagem local ou do servidor.

--- src/main/java/com/algaworks/brewer/config/S3Config.java
+++ src/main/java/com/algaworks/brewer/config/S3Config.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
 import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentials;
@@ -19,16 +18,17 @@ import com.amazonaws.services.s3.AmazonS3Client;
 public class S3Config {
 	
 	@Autowired
-	private Environment env;
-	
+	//private Environment env;
+
 	@Bean
 	public AmazonS3 amazonS3() {
 		AWSCredentials credenciais = new BasicAWSCredentials(
-				env.getProperty("AWS_ACCESS_KEY_ID"), env.getProperty("AWS_SECRET_ACCESS_KEY"));
+				//env.getProperty("AWS_ACCESS_KEY_ID"), env.getProperty("AWS_SECRET_ACCESS_KEY"));
+				"teste", "teste");
 		AmazonS3 amazonS3 = new AmazonS3Client(credenciais, new ClientConfiguration());
 		Region regiao = Region.getRegion(Regions.US_EAST_1);
 		amazonS3.setRegion(regiao);
 		return amazonS3;
 	}
-
+	
 }
